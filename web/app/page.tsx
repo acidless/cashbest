ce5357e1c94b35a9d9a1e1b2c036cf790bf0a364
Oleg <kukunov2004@gmail.com>
2026-01-18 23:34:54 +0300

    fix: added url in env

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,5 +1,7 @@
 import CashbackGroups from "@/app/CashbackGroups";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
     const response = await fetch(`${process.env.HOST}/api/cashback`);
     if (!response.ok) return null;
