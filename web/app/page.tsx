b8d2c32c1b9f599344ed6459f800ae430a313b41
Oleg <kukunov2004@gmail.com>
2026-01-18 23:32:36 +0300

    fix: added url in env

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -2,6 +2,8 @@ import CashbackGroups from "@/app/CashbackGroups";
 
 export default async function Home() {
     const response = await fetch(`${process.env.HOST}/api/cashback`);
+    if (!response.ok) return null;
+
     const cashbackData = await response.json();
 
     return <main className="px-3 py-4">
